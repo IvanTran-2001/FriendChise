360d06b846555afb2d63c00826f3341394a90a7b
Ivan Tran <243293699+IvanTran-2001@users.noreply.github.com>
2026-05-09 22:01:54 +1000

    test: update updateTaskAction unit test to match new revalidation path

--- prisma/migrations/20260507093800_rename_override_to_pinned_output/migration.sql
+++ prisma/migrations/20260507093800_rename_override_to_pinned_output/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `overrideQuantity` on the `ConversionTemplateEntry` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "ConversionTemplateEntry" DROP COLUMN "overrideQuantity",
-ADD COLUMN     "pinnedOutput" DOUBLE PRECISION;
+ALTER TABLE "ConversionTemplateEntry"
+RENAME COLUMN "overrideQuantity" TO "pinnedOutput";
